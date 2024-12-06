55e00a657e25bc8e651f03190fbb76f3aeac6cf1
DavideAvagnano <dav.avagnano@gmail.com>
2024-12-06 09:30:03 +0100

    cambiato font family in poppins e aggiustato il responsive

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -8,8 +8,8 @@ export const Intro = () => {
       id="intro"
       className="max-w-screen-sm min-h-screen flex flex-col items-center justify-center gap-5 text-center px-8 md:p-0 md:pt-40"
     >
-      <h1 className="text-4xl sm:text-5xl md:text-6xl flex items-center justify-center gap-2">
-        <span>
+      <h1 className="text-4xl sm:text-6xl md:text-7xl flex items-center justify-center gap-2">
+        <span className="font-semibold">
           hi, <span className="text-accent">Davide</span> here.
         </span>
         <span className="text-accent motion-preset-blink motion-duration-1500">

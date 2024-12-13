cc724eb844bf52ca25fb630bf68fa66849b46c5e
DavideAvagnano <dav.avagnano@gmail.com>
2024-12-13 21:11:47 +0100

    piccole modifiche, colore destructive, testo centrato in contact section

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -6,7 +6,7 @@ export const Contact = () => {
     <section id="contact" className="section">
       <SectionHeading label="/contact" />
 
-      <p className="pb-10 text-foreground-light">
+      <p className="pb-10 max-w-xl mx-auto text-center text-foreground-light">
         Whether you have a question, or just want to say hi, feel free to drop
         me a message. I'll get back to you as soon as possible.
       </p>
